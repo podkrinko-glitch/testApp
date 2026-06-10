0850c8ad991dac0e5141f56edf5247e739f220fc
podkrinko-glitch <podkrinko27@gmail.com>
2026-06-11 01:33:24 +0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Test Website",
+    layout="wide"
+)
+
+st.title("My Test Website")
+
+st.write("Welcome to my Streamlit test website.")
